293183de9ea02ba89b381cbb53d13237e2b115a8
Maciek Głuszak <maciek.gluszak@mcgluszak.com>
2020-08-21 11:00:36 +0200

    dodałem komentarz ziobro ty kurwo

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
 
 class Channels extends Command {
   static description = 'describe the command here'
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({ char: 'n', description: 'name to print' }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    force: flags.boolean({ char: 'f' }),
   }
 
-  static args = [{name: 'file'}]
-
+  static args = [{ name: 'file' }]
+  console.log('ziobro ty kurwo 2020-08-21')
   async run() {
-    const {args, flags} = this.parse(Channels)
-    
+    const { args, flags } = this.parse(Channels)
+
     const name = flags.name ?? 'world'
     this.log("siema")
     if (args.file && flags.force) {
