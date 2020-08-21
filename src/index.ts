19de98e6854dbb306c4d29244fd333742d48245c
Pawel Gera <pawel.gera@mcgluszak.com>
2020-08-21 10:31:07 +0200

    drugi komit

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,9 @@ class Channels extends Command {
 
   async run() {
     const {args, flags} = this.parse(Channels)
-
+    
     const name = flags.name ?? 'world'
-    this.log(`hello ${name} from .\\src\\index.ts`)
+    this.log("siema")
     if (args.file && flags.force) {
       this.log(`you input --force and --file: ${args.file}`)
     }
